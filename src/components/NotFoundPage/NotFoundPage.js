3d83a9b483aa81c2eed36e2ade30e10ee5b1dbb5
Darya Krinitsyna <krinitsyna.darya.98@mail.ru>
2023-05-20 15:31:19 +0300

    feat: add link back handler

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,14 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 import "./NotFoundPage.css";
 import { Link } from "react-router-dom";
 
 function NotFoundPage() {
+  const navigate = useNavigate();
   return (
     <section className="not-found-page">
       <div className="not-found-page__container">
         <h2 className="not-found-page__title">404</h2>
         <h3 className="not-found-page__subtitle">Страница не найдена</h3>
       </div>
-      <Link to="/" className="not-found-page__link">
+      <Link className="not-found-page__link" onClick={() => navigate(-1)}>
         Назад
       </Link>
     </section>
